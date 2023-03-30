65242adc788568cc74a71fa52f71198b85f08947
gabaish <gabay168@gmail.com>
2023-03-31 00:53:30 +0300

    ignore

---
+++ app/src/main/java/com/example/abilitechapp/ColoringData.java
@@ -0,0 +1,13 @@
+package com.example.abilitechapp;
+
+import java.util.LinkedList;
+
+public class ColoringData {
+
+    String[] family = new String[] {
+            "mom", "dad", "brother", "sister", "love" };
+
+
+
+
+}
